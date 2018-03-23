8054099a03f8596272f4173d2e9ec8c0f16ee585
hmmzhtx <hmmzhtx@aliyun.com>
2018-03-24 00:33:42 +0800

    add hello

---
+++ HelloWord/src/main/java/com/myylm/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.myylm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hmmzhtx
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
