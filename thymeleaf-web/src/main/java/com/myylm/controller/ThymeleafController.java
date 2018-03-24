5185ce69ab52069878581a482fbff4e771a8bcc7
huangming <hmmzhtx@aliyun.com>
2018-03-24 19:00:09 +0800

    添加日志模块

--- thymeleaf-web/src/main/java/com/myylm/controller/ThymeleafController.java
+++ thymeleaf-web/src/main/java/com/myylm/controller/ThymeleafController.java
@@ -1,5 +1,7 @@
 package com.myylm.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ThymeleafController {
 
+
+    protected static Logger logger= LoggerFactory.getLogger(ThymeleafController.class);
+
     /**
      * Thymeleaf
      * @param model
@@ -17,6 +22,8 @@ public class ThymeleafController {
      */
     @RequestMapping("/toThy")
     public String index(Model model){
+        logger.trace("测试1");
+        logger.info("测试2");
         model.addAttribute("name", "11111");
         return "thy";
     }
