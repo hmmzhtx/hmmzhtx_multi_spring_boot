352c5af0a673e1d30e4e2f85e4d34693440a516b
huangming <hmmzhtx@aliyun.com>
2018-03-30 15:02:23 +0800

    ---

--- springframework-web/src/main/java/com/myylm/controller/SpringFrameworkController.java
+++ springframework-web/src/main/java/com/myylm/controller/SpringFrameworkController.java
@@ -3,7 +3,6 @@ package com.myylm.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
--- thymeleaf-web/src/main/java/com/myylm/controller/ThymeleafController.java
+++ thymeleaf-web/src/main/java/com/myylm/controller/ThymeleafController.java
@@ -1,7 +1,5 @@
 package com.myylm.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ThymeleafController {
 
 
-    protected static Logger logger= LoggerFactory.getLogger(ThymeleafController.class);
 
     /**
      * Thymeleaf
@@ -22,13 +19,6 @@ public class ThymeleafController {
      */
     @RequestMapping("/toThy")
     public String index(Model model){
-        //日志级别从低到高分为TRACE < DEBUG < INFO < WARN < ERROR < FATAL，如果设置为WARN，则低于WARN的信息都不会输出。
-        logger.trace("日志输出 trace");
-        logger.debug("日志输出 debug");
-        logger.info("日志输出 info");
-        logger.warn("日志输出 warn");
-        logger.error("日志输出 error");
-
         model.addAttribute("name", "11111");
         return "thy";
     }
