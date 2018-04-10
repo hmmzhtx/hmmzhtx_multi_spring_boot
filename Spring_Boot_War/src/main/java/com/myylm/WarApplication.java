e3625c03e10e924f95b5232c2794abe0464c81af
huangming <hmmzhtx@aliyun.com>
2018-04-10 17:28:30 +0800

    重构

--- Spring_Boot_War/src/main/java/com/myylm/WarApplication.java
+++ Spring_Boot_War/src/main/java/com/myylm/WarApplication.java
@@ -18,6 +18,7 @@ package com.myylm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
@@ -26,6 +27,12 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class WarApplication extends SpringBootServletInitializer {
 
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WarApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WarApplication.class, args);
 	}
