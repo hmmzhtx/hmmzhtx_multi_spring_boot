938206800560695d8bf3c614c7458fd4f2eea82e
huangming <hmmzhtx@aliyun.com>
2019-07-09 21:04:50 +0800

    `````

---
+++ Spring_Boot_Concurrency/src/main/java/com/myylm/controller/ConcurrencyController.java
@@ -0,0 +1,30 @@
+package com.myylm.controller;
+
+import com.myylm.commons.aop.LoggerManage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ccy")
+public class ConcurrencyController {
+
+    public  static Logger logger = LoggerFactory.getLogger(ConcurrencyController.class);
+
+    /**
+     * 返回其 json数据
+     * @return
+     */
+    @RequestMapping(value = "/ccy_test", method = RequestMethod.GET)
+    @ResponseBody
+    @LoggerManage(logDescription = "并发_测试")
+    public String CcyController(){
+        return "并发搭建测试";
+    }
+
+
+
+}
---
+++ Spring_Cloud_Eureka_Server/src/main/java/com/myylm/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.myylm;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer  //该注解表明应用为eureka服务，有可以联合多个服务作为集群，对外提供服务注册以及发现功能
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+    
+}
