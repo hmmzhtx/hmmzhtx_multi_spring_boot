653850fc28f312083ba701ea50b2e04fea19c7b8
huangming <hmmzhtx@aliyun.com>
2019-08-05 18:38:08 +0800

    `````

--- Spring_Cloud_Eureka_Consumer_Feign/src/main/java/com/myylm/service/HelloClientService.java
+++ Spring_Cloud_Eureka_Consumer_Feign/src/main/java/com/myylm/service/HelloClientService.java
@@ -1,7 +1,7 @@
 package com.myylm.service;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.cloud.openfeign.FeignClient;
+        import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient("eureka-client")
 public interface HelloClientService {
