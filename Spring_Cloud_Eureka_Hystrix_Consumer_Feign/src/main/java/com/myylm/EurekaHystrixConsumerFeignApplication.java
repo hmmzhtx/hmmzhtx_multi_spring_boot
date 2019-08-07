8c2f56900945b2838942809570b807420672d6ec
huangming <hmmzhtx@aliyun.com>
2019-08-07 19:22:50 +0800

    `````

--- Spring_Cloud_Eureka_Hystrix_Consumer_Feign/src/main/java/com/myylm/EurekaHystrixConsumerFeignApplication.java
+++ Spring_Cloud_Eureka_Hystrix_Consumer_Feign/src/main/java/com/myylm/EurekaHystrixConsumerFeignApplication.java
@@ -4,12 +4,13 @@ package com.myylm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 带熔断功能的消费者
  */
-//@EnableHystrix  //Feign自带熔断，不需要此注解，开启熔断在配置文件中:feign.hystrix.enabled=true（在应用主类中使用@EnableCircuitBreaker或@EnableHystrix注解开启Hystrix的使用）
+@EnableHystrix//Feign自带熔断，不需要此注解，开启熔断在配置文件中:feign.hystrix.enabled=true（在应用主类中使用@EnableCircuitBreaker或@EnableHystrix注解开启Hystrix的使用）
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
--- Spring_Cloud_Eureka_Hystrix_Dashboard/src/main/java/com/myylm/HystrixDashboardApplication.java
+++ Spring_Cloud_Eureka_Hystrix_Dashboard/src/main/java/com/myylm/HystrixDashboardApplication.java
@@ -4,11 +4,12 @@ import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServl
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 
 @EnableHystrixDashboard
+@EnableDiscoveryClient
 @SpringCloudApplication
 public class HystrixDashboardApplication {
 
