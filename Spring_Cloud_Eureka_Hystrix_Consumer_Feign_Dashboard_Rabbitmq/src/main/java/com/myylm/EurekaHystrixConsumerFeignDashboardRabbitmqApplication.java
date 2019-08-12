f113c9b6c63ec0fc19572e98fe7b9b038c8e82e5
huangming <hmmzhtx@aliyun.com>
2019-08-12 18:29:40 +0800

    `````

--- Spring_Cloud_Eureka_Hystrix_Consumer_Feign_Dashboard_Rabbitmq/src/main/java/com/myylm/EurekaHystrixConsumerFeignDashboardRabbitmqApplication.java
+++ Spring_Cloud_Eureka_Hystrix_Consumer_Feign_Dashboard_Rabbitmq/src/main/java/com/myylm/EurekaHystrixConsumerFeignDashboardRabbitmqApplication.java
@@ -2,12 +2,11 @@ package com.myylm;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
-
 
 
 @EnableHystrix   //@EnableHystrix  如果要实现面板，必须加入此注解，即使fegin已经通过属性
