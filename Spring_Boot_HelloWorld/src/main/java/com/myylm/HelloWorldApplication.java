cbc0a9175830832f75368732829c0278422080ea
huangming <hmmzhtx@aliyun.com>
2019-08-22 10:46:08 +0800

    测试分支

--- Spring_Boot_HelloWorld/src/main/java/com/myylm/HelloWorldApplication.java
+++ Spring_Boot_HelloWorld/src/main/java/com/myylm/HelloWorldApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWorldApplication {
 
+    /*测试分支*/
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
     }
